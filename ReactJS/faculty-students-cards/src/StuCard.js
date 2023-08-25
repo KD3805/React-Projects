4be7036d95a99d6cd4fd5cd2e114407928a390d9
KD3805 <114673669+KD3805@users.noreply.github.com>
2023-08-25 17:24:50 +0530

    Update StuCard.js

--- ReactJS/faculty-students-cards/src/StuCard.js
+++ ReactJS/faculty-students-cards/src/StuCard.js
@@ -2,36 +2,6 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-var stuData = [
-    ["https://cdn.wallpapersafari.com/83/72/DU7VOk.png", "Kishan", 205, "B.Tech CSE", 3, "Sept - 2022"],
-
-    ["https://w0.peakpx.com/wallpaper/28/814/HD-wallpaper-sasuke-uchiha-naruto-sasuke-uchiha.jpg", "Parth", 203, "B.Tech CSE", 3, "Sept - 2022"],
-
-    ["https://cdn.wallpapersafari.com/83/72/DU7VOk.png", "Jenish", 206, "B.Tech CSE", 3, "Sept - 2022"],
-
-    ["https://w0.peakpx.com/wallpaper/28/814/HD-wallpaper-sasuke-uchiha-naruto-sasuke-uchiha.jpg", "Viral", 219, "B.Tech CSE", 3, "Sept - 2022"],
-
-    ["https://cdn.wallpapersafari.com/83/72/DU7VOk.png", "Mayur", 212, "B.Tech CSE", 3, "Sept - 2022"],
-
-    ["https://w0.peakpx.com/wallpaper/28/814/HD-wallpaper-sasuke-uchiha-naruto-sasuke-uchiha.jpg", "Deep", 209, "B.Tech CSE", 3, "Sept - 2022"],
-
-    ["https://cdn.wallpapersafari.com/83/72/DU7VOk.png", "Devang", 213, "B.Tech CSE", 3, "Sept - 2022"],
-
-    ["https://w0.peakpx.com/wallpaper/28/814/HD-wallpaper-sasuke-uchiha-naruto-sasuke-uchiha.jpg", "Aayush", 204, "B.Tech CSE", 3, "Sept - 2022"],
-
-    ["https://cdn.wallpapersafari.com/83/72/DU7VOk.png", "Darshan", 202, "B.Tech CSE", 3, "Sept - 2022"],
-
-    ["https://w0.peakpx.com/wallpaper/28/814/HD-wallpaper-sasuke-uchiha-naruto-sasuke-uchiha.jpg", "Bhavesh", 215, "B.Tech CSE", 3, "Sept - 2022"],
-
-    ["https://cdn.wallpapersafari.com/83/72/DU7VOk.png", "Kishan", 205, "B.Tech CSE", 3, "Sept - 2022"],
-
-    ["https://w0.peakpx.com/wallpaper/28/814/HD-wallpaper-sasuke-uchiha-naruto-sasuke-uchiha.jpg", "Parth", 203, "B.Tech CSE", 3, "Sept - 2022"],
-
-    
-
-];
-
-
 function CardBody(stu) {
   const navigate = useNavigate();
 
